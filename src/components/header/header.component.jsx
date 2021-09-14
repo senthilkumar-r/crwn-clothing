ef9720bdf5e824b180a901c30648b73c7a506552
Senthilkumar Rajendran <r.senthilkumar@outlook.com>
2021-09-14 12:53:10 -0600

    added cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
+import CartIcon from '../cart-icon/cart-icon';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -28,6 +27,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
