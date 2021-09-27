51cb0876b94d407c758f03d249e5ec1ea64b473b
Senthilkumar Rajendran <r.senthilkumar@outlook.com>
2021-09-27 12:41:06 -0600

    updated shop selector and reducer

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors.js';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors.js';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
